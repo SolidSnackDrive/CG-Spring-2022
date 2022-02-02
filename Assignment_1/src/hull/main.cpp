028bf40e4ff3200f11cbaecee56994ebcac0a9f8
Graham Stewart <supergproductions@gmail.com>
2022-02-01 18:38:45 -0800

    Copied in working solutions for Assignment 1 done outside git

--- Assignment_1/src/hull/main.cpp
+++ Assignment_1/src/hull/main.cpp
@@ -7,37 +7,74 @@
 #include <vector>
 ////////////////////////////////////////////////////////////////////////////////
 
+
 typedef std::complex<double> Point;
 typedef std::vector<Point> Polygon;
 
+#define PI 3.14159265
+
 double inline det(const Point &u, const Point &v) {
-	// TODO
-	return 0;
+	return u.real()*v.imag() - u.imag()*v.real();
 }
+
+double inline absangle(Point &a) {
+	double theta = atan2(a.imag(),a.real());
+	if(theta < 0) {
+		return 2*PI - theta;
+	}
+	return theta;
+}
+
 
 struct Compare {
 	Point p0; // Leftmost point of the poly
 	bool operator ()(const Point &p1, const Point &p2) {
-		// TODO
-		return true;
+		Point p01 = Point(p1.real() - p0.real(), p1.imag() - p0.imag());
+		Point p02 = Point(p2.real() - p0.real(), p2.imag() - p0.imag());
+
+		double theta1 = absangle(p01);
+		double theta2 = absangle(p02);
+		return theta1 < theta2;
 	}
 };
 
+
 bool inline salientAngle(Point &a, Point &b, Point &c) {
-	// TODO
-	return false;
+	Point ab = Point(b.real() - a.real(), b.imag() - a.imag());
+	Point bc = Point(c.real() - b.real(), c.imag() - b.imag());
+	double z_component = det(ab, bc);
+	return z_component > 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 Polygon convex_hull(std::vector<Point> &points) {
 	Compare order;
-	// TODO
-	order.p0 = Point(0, 0);
+	Point lowest = *std::min_element(points.begin(), points.end(), [](Point &p1, Point &p2) { return p1.imag() < p2.imag(); } );
+	for(int i = 0; i < points.size(); i++ ) {
+		if(points[i].imag() == lowest.imag() && points[i].real() < lowest.real()) {
+			lowest = points[i];
+		}
+	}
+	order.p0 = lowest;
+
 	std::sort(points.begin(), points.end(), order);
+
 	Polygon hull;
-	// TODO
-	// use salientAngle(a, b, c) here
+
+	for (const auto& v : points) {
+		if(hull.size() < 3) {
+			hull.push_back(v);
+			continue;
+		}
+		
+		while(hull.size() > 2 && !salientAngle(*(hull.end()-3),*(hull.end()-2),*(hull.end()-1))) {
+			hull.erase(hull.end()-2);
+		}
+
+		hull.push_back(v);
+	}
+
 	return hull;
 }
 
@@ -46,7 +83,16 @@ Polygon convex_hull(std::vector<Point> &points) {
 std::vector<Point> load_xyz(const std::string &filename) {
 	std::vector<Point> points;
 	std::ifstream in(filename);
-	// TODO
+	std::string line;
+	std::getline(in, line);
+	while(std::getline(in,line)) {
+		std::istringstream iss(line);
+		double a,b,c;
+		if (!(iss >> a >> b >> c)) {
+			break;
+		}
+		points.push_back(Point(a,b));
+	}
 	return points;
 }
 
--- Assignment_1/src/inside/main.cpp
+++ Assignment_1/src/inside/main.cpp
@@ -11,25 +11,65 @@ typedef std::complex<double> Point;
 typedef std::vector<Point> Polygon;
 
 double inline det(const Point &u, const Point &v) {
-	// TODO
-	return 0;
+	return u.real()*v.imag() - u.imag()*v.real();
+}
+
+bool inline ccw(const Point &a, const Point &b, const Point &c) {
+	Point ab = Point(b.real() - a.real(),b.imag() - a.imag());
+	Point ac = Point(c.real() - a.real(),c.imag() - a.imag());
+	return det(ab,ac) > 0;
 }
 
 // Return true iff [a,b] intersects [c,d], and store the intersection in ans
 bool intersect_segment(const Point &a, const Point &b, const Point &c, const Point &d, Point &ans) {
-	// TODO
-	return true;
+	/*Point ab = Point(b.real() - a.real(),b.imag() - a.imag());
+	Point ac = Point(c.real() - a.real(),c.imag() - a.imag());
+	Point ad = Point(d.real() - a.real(),d.imag() - a.imag());
+
+	double ab_lowY = std::min(a.imag(),b.imag());
+	double ab_highY = std::max(a.imag(),b.imag());
+	double ab_lowX = std::min(a.real(),b.real());
+	double ab_highX = std::max(a.real(),b.real());
+
+	double cd_lowY = std::min(c.imag(),d.imag());
+	double cd_highY = std::max(c.imag(),d.imag());
+	double cd_lowX = std::min(c.real(),d.real());
+	double cd_highX = std::max(c.real(),d.real());
+
+	if(ab_highY < cd_lowY || cd_highY < ab_lowY) return false;
+	if(ab_highX < cd_lowX || cd_highX < ab_lowX) return false;*/
+
+
+	return ccw(a,c,d) != ccw(b,c,d) && ccw(a,b,c) != ccw(a,b,d);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 bool is_inside(const Polygon &poly, const Point &query) {
 	// 1. Compute bounding box and set coordinate of a point outside the polygon
-	// TODO
-	Point outside(0, 0);
+	//Point lowestY = *std::min_element(poly.begin(), poly.end(), [](Point &p1, Point &p2) { return p1.imag() < p2.imag(); } );
+	//Point lowestX = *std::min_element(poly.begin(), poly.end(), [](Point &p1, Point &p2) { return p1.real() < p2.real(); } );
+	double highestY = poly.front().imag();
+	double highestX = poly.front().real();
+	for(size_t i = 0; i < poly.size(); i++) {
+		if(poly[i].imag() > highestY) highestY = poly[i].imag();
+		if(poly[i].real() > highestX) highestX = poly[i].real();
+	}
+
+	Point outside(highestX + 900, highestY + 900);
+	//std::cout << "Highest point was (" << highestX << ", " << highestY << ")\n";
+	
 	// 2. Cast a ray from the query point to the 'outside' point, count number of intersections
-	// TODO
-	return true;
+	Point dummy;
+	int intersects = 0;
+	for(size_t i = 0; i < poly.size() - 1; i++) {
+		std::cout << "Testing against side (" << poly[i].real() << ", " << poly[i].imag() << ") and (" << poly[i+1].real() << ", " << poly[i +1].imag() << ")\n";
+		if(intersect_segment(poly[i], poly[i+1], query, outside, dummy)) intersects++;
+	}
+	if(intersect_segment(poly[0], poly[poly.size() - 1], query, outside, dummy)) intersects++;
+	std::cout << "Testing against side (" << poly[0].real() << ", " << poly[0].imag() << ") and (" << poly[poly.size() - 1].real() << ", " << poly[poly.size() - 1].imag() << ")\n";
+	std::cout << intersects << " intersects for point(" << query.real() << ", " << query.imag() << ")\n";
+	return intersects % 2;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,18 +77,58 @@ bool is_inside(const Polygon &poly, const Point &query) {
 std::vector<Point> load_xyz(const std::string &filename) {
 	std::vector<Point> points;
 	std::ifstream in(filename);
-	// TODO
+	std::string line;
+	std::getline(in, line);
+	while(std::getline(in,line)) {
+		std::istringstream iss(line);
+		double a,b,c;
+		if (!(iss >> a >> b >> c)) {
+			break;
+		}
+		points.push_back(Point(a,b));
+	}
 	return points;
 }
 
 Polygon load_obj(const std::string &filename) {
 	std::ifstream in(filename);
-	// TODO
-	return {};
+	Polygon poly;
+	std::string line;
+	if(!in.is_open()) {
+		throw std::runtime_error("failed to open file " + filename);
+	}
+
+	while(std::getline(in,line)) {
+		std::istringstream iss(line);
+		char t;
+		if(!(iss >> t) || t == 'f') {
+			break;
+		}
+		double a, b, c;
+		if(!(iss >> a >> b >> c)) {
+			break;
+		}
+
+		poly.push_back(Point(a,b));
+	}
+
+	for (const auto &v : poly) {
+		std::cout << "Read point (" << v.real() << ", " << v.imag() <<  ")\n";
+	}
+	return poly;
 }
 
 void save_xyz(const std::string &filename, const std::vector<Point> &points) {
-	// TODO
+	std::ofstream out(filename);
+	if (!out.is_open()) {
+		throw std::runtime_error("failed to create file " + filename);
+	}
+	out << std::fixed;
+	out << points.size() << "\n";
+	for (const auto &v : points) {
+		out << v.real() << ' ' << v.imag() << " 0\n";
+	}
+	out << std::endl;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
