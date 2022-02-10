03be791a3431f5ae9f5bdc15c20a4b2fe724abd0
Graham Stewart <supergproductions@gmail.com>
2022-02-10 12:06:37 -0800

    Ortho pgram intersection working for both halfs, no shading

--- Assignment_2/src/main.cpp
+++ Assignment_2/src/main.cpp
@@ -72,13 +72,19 @@ void raytrace_sphere()
     write_matrix_to_png(C, C, C, A, filename);
 }
 
+bool uvt_check(double u, double v, double t) {
+    return t > 0 && u >= 0 && v >= 0 && (u + v <= 1);
+}
+
 void raytrace_parallelogram()
 {
     std::cout << "Simple ray tracer, one parallelogram with orthographic projection" << std::endl;
-
+    
     const std::string filename("plane_orthographic.png");
     MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
     MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
+
+    
 
     // The camera is orthographic, pointing in the direction -z and covering the
     // unit square (-1,1) in x and y
@@ -88,14 +94,17 @@ void raytrace_parallelogram()
 
     // TODO: Parameters of the parallelogram (position of the lower-left corner +
     // two sides)
-    Vector3d pgram_origin (-1,0,-3);
-    Vector3d pgram_u (1,1);
-    Vector3d pgram_v (1,-1);
+    Vector3d pgram_origin (-0.5,-0.5,0);
+    Vector3d pgram_u (0,0.7,-10);
+    Vector3d pgram_v (1,0.4,0);
+
+   std::cerr << "B4 loop\n\n";
 
     Vector3d planeNormal = pgram_u.cross(pgram_v);
     double planeDist = -planeNormal.dot(pgram_origin);
 
-
+                //double t = -(planeNormal.dot(ray_origin) + planeDist) / planeNormal.dot(ray_vector);
+            //Vector3d intersect = ray_origin + t*ray_vector;
 
     // Single light source
     const Vector3d light_position(-1, 1, 1);
@@ -105,37 +114,49 @@ void raytrace_parallelogram()
         for (unsigned j = 0; j < C.rows(); ++j)
         {
             // Prepare the ray
+
             Vector3d ray_origin = origin + double(i) * x_displacement + double(j) * y_displacement;
             Vector3d ray_direction = RowVector3d(0, 0, -1);
 
             Vector3d ray_vector = Vector3d(ray_origin(0),ray_origin(1),ray_direction(2) * 999) - ray_origin;
 
             // TODO: Check if the ray intersects with the parallelogram
-            //double t = -(planeNormal.dot(ray_origin) + planeDist) / planeNormal.dot(ray_vector);
-            //Vector3d intersect = ray_origin + t*ray_vector;
 
-
-            /*
+            /*;
                 a + u(b-a) + v(c-a) = e + td
                 u(b-a) + v(c-a) - td = (e - a)
             */
 
-           Matrix3d linA;
-           linA << pgram_u, pgram_v, (-1* ray_vector);
+            Matrix3d linA;
+            Vector3d n_ray_vector = -1 * ray_vector;
+            linA << pgram_u, pgram_v, n_ray_vector;
 
-           Vector3d linb = ray_origin - pgram_origin;
+            Vector3d pgram_other_origin = pgram_origin + pgram_v + pgram_u;
 
-           Vector3d linx = linA.colPivHouseholderQr().solve(linb);
+            Vector3d linb = ray_origin - pgram_origin;
 
+            Vector3d linx = linA.partialPivLu().solve(linb);
 
-            if (linx(2) > 0 && linx(0) >= 0 && linx(1) >= 1 && (linx(0) + linx(1) <= 1))
+            Matrix3d linC;
+            linC << -1 * pgram_u, -1 * pgram_v, n_ray_vector;
+
+            Vector3d lind = ray_origin - pgram_other_origin;
+
+            lind = linC.partialPivLu().solve(lind);
+           //  std::cerr << "Point (" << i << ", " << j << " ) -- " << linx << "\n\n";
+
+            bool inter1 = uvt_check(linx(0),linx(1),linx(2));
+            bool inter2 = uvt_check(lind(0),lind(1),lind(2));
+
+            if (inter1 || inter2) /* t > 0, u,v >= 0, u + v <= 1 */
             {
+                // std::cerr << "HIT!!\n";
                 // TODO: The ray hit the parallelogram, compute the exact intersection
                 // point
-                Vector3d ray_intersection = linx(2) * ray_vector;
+                Vector3d ray_intersection = inter1 ? linx(2) * ray_vector : lind(2) * ray_vector;
 
                 // TODO: Compute normal at the intersection point
-                Vector3d ray_normal = ray_intersection.normalized();
+                Vector3d ray_normal = -1 * pgram_u.normalized().cross(pgram_v.normalized());
 
                 // Simple diffuse model
                 C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
@@ -277,7 +298,7 @@ void raytrace_shading()
 
 int main()
 {
-    raytrace_sphere();
+   // raytrace_sphere();
     raytrace_parallelogram();
     raytrace_perspective();
     raytrace_shading();
