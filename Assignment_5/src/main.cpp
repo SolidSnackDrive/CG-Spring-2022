35c0ba771a3ba5ece491f4b408c893822dfd61d0
Graham Stewart <supergproductions@gmail.com>
2022-04-13 10:53:05 -0700

    implemented red bunny and wireframe

--- Assignment_5/src/main.cpp
+++ Assignment_5/src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 // Utilities for the Assignment
 #include "raster.h"
@@ -10,10 +11,56 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION // Do not include this line twice in your project!
 #include "stb_image_write.h"
 
+#define DATA_DIR "/home/gstew/Documents/school/csc305/CG-Spring-2022/Assignment_4/data/"
+
 using namespace std;
+using namespace Eigen;
+
+MatrixXd vertices;
+MatrixXi facets;
+
+const string data_dir = DATA_DIR;
+const string mesh_filename(data_dir + "bunny.off");
+vector<VertexAttributes> mesh_vertices; 
+vector<VertexAttributes> line_vertices; 
+
+void load_mesh() {
+    ifstream in(mesh_filename);
+    string token;
+    in >> token;
+    int nv, nf, ne;
+    in >> nv >> nf >> ne;
+    vertices.resize(nv,3);
+    facets.resize(nf, 3);
+    for (int i = 0; i < nv; ++i)
+    {
+        in >> vertices(i, 0) >> vertices(i, 1) >> vertices(i, 2);
+    }
+    for (int i = 0; i < nf; i++) {
+        int s;
+        in >> s >> facets(i,0) >> facets(i,1) >> facets(i,2);
+        assert(s==3);
+    }
+    for (int i = 0; i < facets.rows(); i++) {
+        Vector3i r = facets.row(i);
+        for (int j = 0; j < 3; j ++) {
+            VertexAttributes v(vertices(r(j), 0), vertices(r(j), 1), vertices(r(j), 2));
+            mesh_vertices.push_back(v);
+        }
+    }
+    for (int i = 0; i < facets.rows(); i++) {
+        Vector3i r = facets.row(i);
+        for (int j = 0; j < 2; j ++) {
+            VertexAttributes v(vertices(r(j), 0), vertices(r(j), 1));
+            line_vertices.push_back(v);
+        }
+    }
+
+}
 
 int main()
 {
+    load_mesh();
 
     // The Framebuffer storing the image rendered by the rasterizer
     Eigen::Matrix<FrameBufferAttributes, Eigen::Dynamic, Eigen::Dynamic> frameBuffer(500, 500);
@@ -45,7 +92,8 @@ int main()
     vertices.push_back(VertexAttributes(1, -1, 0));
     vertices.push_back(VertexAttributes(0, 1, 0));
 
-    rasterize_triangles(program, uniform, vertices, frameBuffer);
+    rasterize_triangles(program, uniform, mesh_vertices, frameBuffer);
+    //rasterize_lines(program, uniform, line_vertices, 0.5f, frameBuffer);
 
     vector<uint8_t> image;
     framebuffer_to_uint8(frameBuffer, image);
